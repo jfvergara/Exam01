650e8b608beb7e4a2563e87a0db01f807e17f3c0
Jose Francisco Vergara Espinoza <j.f.vergara@cgiar.org>
2020-12-14 18:00:18 -0600

    Add project files.

---
+++ DriverHelper.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+
+namespace Exam01
+{
+    public class DriverHelper
+    {
+        public static IWebDriver Driver { get; set; }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+
+namespace Exam01
+{
+    public class Tests : DriverHelper
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--start-maximized");
+            Driver = new ChromeDriver(chromeOptions);
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://www.commscope.com/");
+            var searchBox = Driver.FindElement(By.XPath("//input[@id='desktop-search-bar']"));
+            searchBox.SendKeys("ME-7000");
+            var searchButton = Driver.FindElement(By.CssSelector("button[aria-label='Search'] > .icon"));
+            searchButton.Click();
+
+            var elements = Driver.FindElements(By.PartialLinkText("ME-7000"));
+
+            foreach (var item in elements)
+            {
+
+                Console.WriteLine(item.Text);
+
+            }
+
+
+
+            Assert.Pass();
+        }
+    }
+}
